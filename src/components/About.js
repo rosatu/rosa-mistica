092d216f8e107e6ebaeada2ab2d17281b3e8a7a3
rosatu <rosalina.tu@gmail.com>
2019-01-01 14:12:50 -0500

    text and drop shadow over homepage img

--- src/components/About.js
+++ src/components/About.js
@@ -4,15 +4,14 @@ export default class About extends Component{
   render(){
     return (
       <React.Fragment>
-        <h1>About Rosa Mistica</h1>
-        <p>Hostal Rosa Mistica is a magical, beachfront hostel with good vibes and even better sunsets. We’re conveniently located in Punta Montanita, a magical place known for its surfing and sunsets. XX minutes from XXXX, Hostal Rosa Mistica offers the best of both worlds--convenience with seclusion, and relaxation with no shortage of things to do!</p>
 
-        <p>Lounge on the beach, visit a nearby waterfall, relax in a hammock in our palm garden, enjoy live music and delicious meals in our restaurant, and meet people from all over the world.</p>
+        <h1>Your Hosts - Frank and Christina</h1>
 
-        <p>You will leave Hostal Rosa Mistica rejuvenated, inspired, and refreshed.</p>
+        <p>Your hosts Frank and Christina are happy to welcome you to Hostal Rosa Mistica, where everyone is family! Frank (originally from the States) and Christina (originally from Peru) fell love with Equador’s natural beauty, biodiversity, and incredible people. After traveling South America for five years, they knew they had found somewhere special when they arrived in Montanita. They have been operating the hostel since XXXXX, 2018.</p>
 
-        <p>Suspendisse elementum vehicula risus sit amet efficitur. Sed dictum ullamcorper velit, sit amet malesuada magna. Phasellus convallis luctus ante, eu hendrerit nisi faucibus ut. In consequat lectus nibh. Suspendisse vitae dui sed ante egestas sagittis non non erat. Praesent placerat augue eget sagittis porta. Morbi at purus quis lacus sollicitudin pharetra quis vitae lacus. Sed urna ante, tempor quis sapien id, rutrum congue eros. Proin porttitor erat lacus, non efficitur neque euismod a. Nunc sed mattis leo. Curabitur bibendum sollicitudin velit id euismod. Donec pellentesque, odio vel finibus pellentesque, diam urna ullamcorper nulla, a ornare arcu ante in tellus. Nam malesuada semper facilisis. Maecenas mollis dolor vel suscipit pharetra.</p>
-      </React.Fragment>
+        <p>Frank and Christina are available on site. Making your stay easy, relaxing, and fun is their top priority, and they can’t wait to meet you.</p>
+
+        </React.Fragment>
     )
   }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,16 @@ import React, {Component} from 'react';
 export default class Home extends Component{
   render(){
     return <React.Fragment>
-            <a className="img">
-              <img src="https://ui.cltpstatic.com/places/hotels/1061/1061960/images/44214086_w.jpg" alt="Hotel Front"></img>
-            </a>
+              <div class="container">
+                <img src="https://ui.cltpstatic.com/places/hotels/5959/595972/images/7252756_w.jpg" alt="Hotel Front"/>
+                <div class="centered"><h1>About Rosa Mistica</h1>
+                <p>Hostal Rosa Mistica is a magical, beachfront hostel with good vibes and even better sunsets. We’re conveniently located in Punta Montanita, a magical place known for its surfing and sunsets. XX minutes from XXXX, Hostal Rosa Mistica offers the best of both worlds--convenience with seclusion, and relaxation with no shortage of things to do!</p>
+
+                <p>Lounge on the beach, visit a nearby waterfall, relax in a hammock in our palm garden, enjoy live music and delicious meals in our restaurant, and meet people from all over the world.</p>
+
+                <p>You will leave Hostal Rosa Mistica rejuvenated, inspired, and refreshed.</p>
+              </div>
+              </div>
           </React.Fragment>
   }
 
