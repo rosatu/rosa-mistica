6cb34ff429f0c9360a02235db00c37eea30fa002
rosatu <rosalina.tu@gmail.com>
2019-01-14 11:22:31 -0500

    removed messaging

--- src/components/About.js
+++ src/components/About.js
@@ -4,32 +4,7 @@ export default class About extends Component{
   render(){
     return (
       <React.Fragment>
-
-        <h1>Your Hosts - Frank and Christina</h1>
-
-        <p>Your hosts Frank and Christina are happy to welcome you to Hostal Rosa Mistica, where everyone is family! Frank (originally from the States) and Christina (originally from Peru) fell love with Equador’s natural beauty, biodiversity, and incredible people. After traveling South America for five years, they knew they had found somewhere special when they arrived in Montanita. They have been operating the hostel since XXXXX, 2018.</p>
-
-        <p>Frank and Christina are available on site. Making your stay easy, relaxing, and fun is their top priority, and they can’t wait to meet you.</p>
-
-        <h1>Location and Activities</h1>
-
-        <p>Montañita is a magical place known for its sunsets, surfing, and relaxed bohemian lifestyle. Enjoy its fine sand beaches and surfing or have a night out sampling the town’s vibrant restaurants and nightlife.</p>
-
-        <p>There are many activities to enjoy, including:</p>
-        <ol>
-          <li>Surfing</li>
-          <li>Yoga</li>
-          <li>
-            Waterfall day trip
-          </li>
-          <li>
-            Visiting nearby village Manglaralto (you can walk there!)
-          </li>
-          <li>
-            ..and of course, eating and drinking (ceviche, anyone?)
-          </li>
-        </ol>
-        <p>Frank and Christina are available to help you plan your activities and connect with local guides. </p>
+        <img src=""></img>
         </React.Fragment>
     )
   }
